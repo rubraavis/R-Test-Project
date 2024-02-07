5dd4cf8c53300fd8030a186b6c7e2cdb869900af
rubraavis <bohdanapalamarchuk@Bohdanas-MacBook-Air.local>
2024-02-07 10:45:31 +0100

    first commit

--- test.R
+++ test.R
@@ -1,2 +1,8 @@
-setwd("~/R-Test-Project")
-git init
+# IN THE TERMINAL - Add project to Git on your local computer
+# git init
+# git add test.R (OR) git add .
+# git commit -m "first commit"
+# git config --global user.name "rubraavis"
+git remote add origin https://github.com/rubraavis/R-Test-Project.git
+git branch -M main
+git push -u origin main
